020a54cd6d0eb616a5433577e6148583e0d155ce
acodeer <acodeer@github.com>
2025-09-11 14:34:20 +0900

    html_commit

---
+++ script.js
@@ -0,0 +1,34 @@
+let count = 0;
+
+const countEl = document.getElementById("count");
+const incBtn = document.getElementById("inc");
+const decBtn = document.getElementById("dec");
+const resetBtn = document.getElementById("reset");
+const squareBtn = document.getElementById("square");
+
+
+function render() {
+  countEl.textContent = count;
+}
+
+incBtn.addEventListener("click", () => {
+  count++;
+  render();
+});
+
+decBtn.addEventListener("click", () => {
+  count--;
+  render();
+});
+
+resetBtn.addEventListener("click", () => {
+  count = 0;
+  render();
+});
+
+squareBtn.addEventListener("click", () => {
+  count = count * count;
+  render();
+});
+
+render();
